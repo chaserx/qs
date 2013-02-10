c6ae9b3b09db852b22e9f00714bb4c4d8ac04370
Chase Southard <chase.southard@gmail.com>
2013-02-09 21:45:22 -0500

    initial commit

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,16 @@
+class InfoController < ApplicationController
+  def index
+  end
+
+  def welcome
+  end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qs::Application.config.secret_token = '61548ff9883975074bec75053e01b92038409c0c00d9cd29469cbe5b5fa8ad7f0cc7565172f804a7aa0fabc0cccdf68e8d81a4265c9aea71e073a3aee791879e'
