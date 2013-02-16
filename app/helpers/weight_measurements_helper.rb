7d27ae0e5d8faa639102eb5a5a447b7aa64e69b0
Chase Southard <chase.southard@gmail.com>
2013-02-15 23:56:12 -0500

    save all weight measurements as kg

--- app/helpers/weight_measurements_helper.rb
+++ app/helpers/weight_measurements_helper.rb
@@ -1,2 +1,12 @@
 module WeightMeasurementsHelper
+  # TODO(chase): should have a method to display the weight records 
+  # consistently according to the user's preference. 
+
+  def show_in_lbs
+    
+  end
+
+  def show_in_kg
+    
+  end
 end
--- app/models/weight_measurement.rb
+++ app/models/weight_measurement.rb
@@ -1,3 +1,27 @@
+# Weight Measurements
+#
+# Record weight either in kilogram or pound, but save all data as kilogram.
+# Data will be displayed to the user using the unit of their choice.
+#
 class WeightMeasurement < ActiveRecord::Base
   attr_accessible :unit, :value
+
+  before_save :convert_to_kg
+
+  def kg_to_lbs
+    value * 2.20462
+  end
+
+  def lbs_to_kg
+    value * 0.453592
+  end
+
+  def convert_to_kg
+    if self.unit == 'lbs'
+      self.value = lbs_to_kg
+      self.unit = 'kg' 
+      return true
+    end
+  end
+
 end
